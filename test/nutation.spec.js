c00ea5b990980857e2946a6b1d3ec394ec7e10be
lax <756993539@qq.com>
2022-08-08 23:41:36 +0800

    add test

--- test/nutation.spec.js
+++ test/nutation.spec.js
@@ -4,7 +4,7 @@
  * @Author: lax
  * @Date: 2022-06-29 21:00:45
  * @LastEditors: lax
- * @LastEditTime: 2022-08-06 09:39:21
+ * @LastEditTime: 2022-08-08 23:36:56
  * @FilePath: \nutation\test\nutation.spec.js
  */
 const Nutation = require("@/nutation.js");
@@ -26,5 +26,30 @@ describe("Nutation IAU1980", () => {
 	});
 	it(`calc O`, () => {
 		expect(nutation.getO()).toBe(-1809.0938095777778);
+	});
+});
+
+describe("《Astronomical.Algorithms》21.a / jde=2446895.5", () => {
+	const jde = 2446895.5;
+	it(`T -0.127296372348`, () => {
+		expect(new Nutation(jde).T).toBeCloseTo(-0.127296372348, 12);
+	});
+	it(`D -56383°.0377`, () => {
+		expect(new Nutation(jde).D).toBeCloseTo(-56383.0377, 3);
+	});
+	it(`l\` -4225°.0208`, () => {
+		expect(new Nutation(jde).l_).toBeCloseTo(-4225.0208, 3);
+	});
+	it(`l -60610°.7216`, () => {
+		expect(new Nutation(jde).l).toBeCloseTo(-60610.7216, 3);
+	});
+	it(`F -61416°.5921`, () => {
+		expect(new Nutation(jde).F).toBeCloseTo(-61416.5921, 3);
+	});
+	it(`O 371°.2531`, () => {
+		expect(new Nutation(jde).O).toBeCloseTo(371.2531, 3);
+	});
+	it(`longitude 9".443`, () => {
+		expect(new Nutation(jde).longitude()).toBeCloseTo(0.0026230555, 3);
 	});
 });
