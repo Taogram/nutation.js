a345e54de8c502280ddccf99a6bc30bfacea86e6
lax <756993539@qq.com>
2022-08-28 11:34:07 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@
  * @Author: lax
  * @Date: 2020-10-22 18:58:04
  * @LastEditors: lax
- * @LastEditTime: 2021-12-19 20:39:07
+ * @LastEditTime: 2022-08-28 11:33:06
  * @FilePath: \tao_solar_terms\.eslintrc.js
  */
 module.exports = {
@@ -18,23 +18,12 @@ module.exports = {
 		"airbnb-base/legacy",
 		"plugin:prettier/recommended",
 		"plugin:jest/recommended",
+		"eslint-config-lax",
 	],
 	plugins: ["prettier"],
 	parserOptions: {
 		ecmaVersion: 11,
 		sourceType: "module",
-	},
-	rules: {
-		"no-bitwise": 0,
-		"no-plusplus": 0,
-		"no-underscore-dangle": 0,
-		"prefer-destructuring": 0,
-		"no-unused-expressions": 0,
-		"no-nested-ternary": 0,
-		"array-callback-return": 0,
-		"class-methods-use-this": 0,
-		"no-param-reassign": 0,
-		camelcase: 0,
 	},
 	overrides: [
 		{
